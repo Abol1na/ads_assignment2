26502339c4b0f749a3c1346692712ee0ea69d817
Abolina667 <abai.sagizhan.absi@gmail.com>
2023-04-13 09:27:36 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(5);
+        arrayList.add(15);
+        arrayList.add(25);
+        arrayList.add(35);
+        arrayList.add(45);
+        arrayList.add(55);
+        System.out.println(arrayList.getElement(5));
+        System.out.println(arrayList.getSize());
+
+
+
+        arrayList.clear();
+
+        System.out.println(arrayList.);
+    }
+
+}
