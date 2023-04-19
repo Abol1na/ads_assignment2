afa98d03f4fadb37d017cb9e66f242485a1f881f
Abolina667 <abai.sagizhan.absi@gmail.com>
2023-04-19 14:15:35 +0600

    test the ArrList

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,38 @@
-import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        MyArrayList<Integer> myList = new MyArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        myList.add(7);
+        myList.add(4);
+        myList.add(1);
+        myList.add(9);
+        myList.add(6);
+        myList.add(5);
+        for (int i = 0; i< myList.size();i++){
+            System.out.print(myList.get(i)+ " ");
+        }
+        System.out.println();
+        myList.sort();
+        for (int i = 0; i< myList.size();i++){
+            System.out.print(myList.get(i)+ " ");
+        }
+        System.out.println();
+        System.out.println(myList.size());
+        System.out.println(myList.contains(1));
+        System.out.println(myList.contains(2));
+        myList.add(8, 2);
+        for (int i = 0; i< myList.size();i++){
+            System.out.print(myList.get(i)+ " ");
+        }
+        System.out.println();
+        myList.remove(2);
+        for (int i = 0; i< myList.size();i++){
+            System.out.print(myList.get(i)+ " ");
+        }
+        System.out.println();
+        System.out.println(myList.indexOf(6));
+        System.out.println(myList.lastIndexOf(6));
     }
 }
