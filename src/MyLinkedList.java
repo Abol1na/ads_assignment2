4a6fc89684ead4855a6a8c1eca552c8757423ec7
Abolina667 <abai.sagizhan.absi@gmail.com>
2023-04-19 22:39:03 +0600

    Linked List first part

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,3 +1,108 @@
-public class MyLinkedList {
+public class MyLinkedList<T> implements MyList<T> {
+    private class Node {
+        T element;
+        Node next;
+        Node prev;
 
+        public Node(T element) {
+            this.element = element;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public MyLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+        @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    public void  add(T element){
+        if(size == arr.length){
+            increaseBuffer();
+        }
+        arr[size++] = element;
+    }
+
+    @Override
+    public void add(T item, int index) {
+
+    }
+
+    @Override
+    public boolean remove(T item) {
+        return false;
+    }
+
+    @Override
+    public T remove(int index) {
+        return null;
+    }
+
+    public void increaseBuffer(){
+        T[] newArr = (T[]) new Object[arr.length*2];
+        for(int i=0; i< arr.length; i++){
+            newArr[i]=arr[i];
+        }
+        arr = newArr;
+    }
+
+    public T getElement(int index) {
+        checkIndex(index);
+        return arr[index];
+    }
+
+    public int getSize() {
+        return size;
+    }
+    public void clear(){
+        this.arr = (T[]) new Object[5];
+        this.size = 0;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public void sort() {
+
+    }
+
+    public void delete(int index){
+        checkIndex(index);
+        for(int i= index + 1; i<size; i++){
+            arr[i-1] = arr[i];
+        }
+        size--;
+    }
+    public void checkIndex(int index){
+        if(index < 0 || index>=size){
+            throw new IndexOutOfBoundsException();
+        }
+    }
 }
